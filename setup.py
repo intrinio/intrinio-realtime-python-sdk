0db9d0a893d5f7433ab123b390b5527264682d3a
Alex Solo <alex.n.solo@gmail.com>
2017-09-08 11:17:17 -0600

    fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
     name = 'intriniorealtime',
-    version = '1.0.0',
+    packages = ['intriniorealtime'],
+    version = '1.0.1',
     author = 'Intrinio Python SDK for Real-Time Stock Prices',
     author_email = 'success@intrinio.co',
     url = 'https://intrinio.com',
-    description = 'Intrinio ',
-    long_description = open('README.md').read().strip(),
-    packages = ['intriniorealtime',],
+    description = 'Intrinio Python SDK for Real-Time Stock Prices',
+    long_description = readme(),
     install_requires = ['requests','websocket-client'],
     download_url = 'https://github.com/intrinio/intrinio-realtime-python-sdk/archive/1.0.0.tar.gz',
     keywords = ['realtime','stock prices','intrinio','stock market','stock data','financial'],
