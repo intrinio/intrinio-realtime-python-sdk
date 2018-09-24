14cc6d4068f20b21b3edfa291526526bd776eb32
Alex Solo <alex.n.solo@gmail.com>
2018-09-24 13:28:13 -0600

    require specific versions of dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url = 'https://intrinio.com',
     description = 'Intrinio Python SDK for Real-Time Stock Prices',
     long_description = readme(),
-    install_requires = ['requests','websocket-client'],
+    install_requires = ['requests=2.19.1','websocket-client==0.48.0'],
     python_requires = '~=3.6',
     download_url = 'https://github.com/intrinio/intrinio-realtime-python-sdk/archive/v2.0.0.tar.gz',
     keywords = ['realtime','stock prices','intrinio','stock market','stock data','financial'],
