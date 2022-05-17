100ef65e7ced90015042adcdbfd0ffbfe10b6f22
Shawn Snyder <ssnyder@intrinio.com>
2022-05-17 16:28:57 -0500

    update download link

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description_content_type = 'text/markdown',
     install_requires = ['requests>=2.26.0','websocket-client>=1.2.1','wsaccel>=0.6.3'],
     python_requires = '~=3.6',
-    download_url = 'https://github.com/intrinio/intrinio-realtime-python-sdk/archive/v3.1.0.tar.gz',
+    download_url = 'https://github.com/intrinio/intrinio-realtime-python-sdk/archive/v4.1.0.tar.gz',
     keywords = ['realtime','stock prices','intrinio','stock market','stock data','financial'],
     classifiers = [
         'Intended Audience :: Financial and Insurance Industry',
