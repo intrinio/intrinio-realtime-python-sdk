bb222f558324802f0f01e8f59afff76c9c5b9aed
Shawn Snyder <ssnyder@intrinio.com>
2023-01-24 21:54:43 -0600

    bump version

--- intriniorealtime/client.py
+++ intriniorealtime/client.py
@@ -183,7 +183,7 @@ class IntrinioRealtimeClient:
             time.sleep(1)
 
     def refresh_token(self):
-        headers = {'Client-Information': 'IntrinioPythonSDKv4.2.0'}
+        headers = {'Client-Information': 'IntrinioPythonSDKv4.2.1'}
         if self.api_key:
             response = requests.get(self.auth_url(), headers=headers)
         else:
