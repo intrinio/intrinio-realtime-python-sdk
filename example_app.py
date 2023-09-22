7e086d8fe72c6026f007c78cfb47eaa3cd535b6e
Shawn Snyder <ssnyder@intrinio.com>
2023-09-22 13:31:37 -0500

    Add sample replay client to example

--- example_app.py
+++ example_app.py
@@ -5,6 +5,7 @@ import sys
 from threading import Timer,Thread,Event
 from intriniorealtime.client import IntrinioRealtimeClient
 from intriniorealtime.replay_client import IntrinioReplayClient
+import datetime
 
 trade_count = 0
 ask_count = 0
@@ -43,15 +44,15 @@ class Summarize(threading.Thread):
 
 options = {
     'api_key': 'API_KEY_HERE',
-    'provider': 'REALTIME',  # 'REALTIME' or DELAYED_SIP or NASDAQ_BASIC
-    'replay_date': None,
-    'with_simulated_delay': False,
-    'delete_file_when_done': True
+    'provider': 'REALTIME'  # 'REALTIME' or DELAYED_SIP or NASDAQ_BASIC
+    # ,'replay_date': datetime.date.today()  # needed for ReplayClient
+    # ,'with_simulated_delay': False  # needed for ReplayClient
+    # ,'delete_file_when_done': True  # needed for ReplayClient
 }
 
 
-#client = IntrinioRealtimeClient(options, on_trade, on_quote)
-client = IntrinioReplayClient(options, on_trade, on_quote)
+client = IntrinioRealtimeClient(options, on_trade, on_quote)
+# client = IntrinioReplayClient(options, on_trade, on_quote)
 stop_event = Event()
 
 
