2961ed9246d7c192acd853056587e11c9403a8a0
Shawn Snyder <ssnyder@intrinio.com>
2023-09-22 13:39:57 -0500

    version bump

--- intriniorealtime/client.py
+++ intriniorealtime/client.py
@@ -27,7 +27,7 @@ DEBUGGING = not (sys.gettrace() is None)
 HEADER_MESSAGE_FORMAT_KEY = "UseNewEquitiesFormat"
 HEADER_MESSAGE_FORMAT_VALUE = "v2"
 HEADER_CLIENT_INFORMATION_KEY = "Client-Information"
-HEADER_CLIENT_INFORMATION_VALUE = "IntrinioPythonSDKv5.0.0"
+HEADER_CLIENT_INFORMATION_VALUE = "IntrinioPythonSDKv5.1.0"
 
 
 class Quote:
