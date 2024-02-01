0ec2c1c5e34451057534d29f1c5857a096ec9296
ssnyder <ssnyder@intrinio.com>
2024-02-01 12:11:39 -0600

    move comma to be consistent

--- example_app.py
+++ example_app.py
@@ -46,13 +46,13 @@ class Summarize(threading.Thread):
 
 options = {
     'api_key': 'API_KEY_HERE',
-    'provider': 'REALTIME',  # 'REALTIME' or DELAYED_SIP or NASDAQ_BASIC
+    'provider': 'REALTIME'  # 'REALTIME' or DELAYED_SIP or NASDAQ_BASIC
     # ,'replay_date': datetime.date.today() - datetime.timedelta(days=1)  # needed for ReplayClient. The date to replay.
     # ,'with_simulated_delay': False  # needed for ReplayClient. This plays back the events at the same rate they happened in market.
     # ,'delete_file_when_done': True  # needed for ReplayClient
     # ,'write_to_csv': False  # needed for ReplayClient
     # ,'csv_file_path': 'data.csv'  # needed for ReplayClient
-    # 'bypass_parsing': True # if you want to handle parsing yourself, set this to True. Otherwise, leave it alone.
+    # ,'bypass_parsing': True # if you want to handle parsing yourself, set this to True. Otherwise, leave it alone.
 }
 
 
