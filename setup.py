6205623f546404a61d2df9d18f8c885ef3f36f30
ssnyder <ssnyder@intrinio.com>
2024-02-01 12:09:12 -0600

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
     name = 'intriniorealtime',
     packages = ['intriniorealtime'],
-    version = '5.2.0',
+    version = '5.2.1',
     author = 'Intrinio Python SDK for Real-Time Stock Prices',
     author_email = 'success@intrinio.com',
     url = 'https://intrinio.com',
